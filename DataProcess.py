0ba1ff5681e668644b365c53caf04d8d680694a8
Phong Do <Donguyenphong@icloud.com>
2024-10-21 21:41:03 -0700

    Changed the categorical data into ints for the ML

--- DataProcess.py
+++ DataProcess.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import seaborn
+import matplotlib
 
 # Turns the CSV into a dataframe named stroke
 stroke = pd.read_csv('healthcare-dataset-stroke-data - healthcare-dataset-stroke-data.csv')
@@ -9,10 +11,25 @@ stroke = stroke.drop(['id'], axis = 1)
 
 # Removing N/A from the BMI data and inputing the mean
 Bmi_Mean = stroke['bmi'].mean()
-print(Bmi_Mean)
 stroke.replace(['N/A'], np.nan, inplace=False)
 stroke['bmi'] = stroke['bmi'].fillna(Bmi_Mean, inplace = False)
 
 #stroke['work_type'] = pd.Categorical(stroke['work_type'])
 #print(stroke['work_type'].code)
 
+# Turning gender into a number, 0 = male, 1 = female
+stroke['gender'] = stroke['gender'].replace({'Male': 0, 'Female': 1})
+
+# Turning ever_married into a number, 0 = Yes , 1 = No
+stroke['ever_married'] = stroke['ever_married'].replace({'Yes': 0, 'No': 1})
+
+# Turning work_type into a number, 0 = child , 1 = never_worked 2 = self-employed, 3 = private, 4 = goverment job
+stroke['work_type'] = stroke['work_type'].replace({'children': 0, 'Never_worked': 1, 'Self-employed': 2, 'Private': 3, 'Govt_job': 4})
+ 
+# Turning residence_type into a number, 0 = urban, 1 = rural
+stroke['Residence_type'] = stroke['Residence_type'].replace({'Urban': 0, 'Rural': 1})
+
+# Turning smoking_status into a number, 0 = never smoked, 1 = unknown, 2 = formerly smokes, 3 = smokes
+stroke['smoking_status'] = stroke['smoking_status'].replace({'never smoked': 0, 'Unknown': 1, 'formerly smoked': 2, 'smokes': 3})
+
+print(stroke.head(10))
--- healthcare-dataset-guide.mk
+++ healthcare-dataset-guide.mk
@@ -1,12 +1,37 @@
-| Gender | Age | Hypertension | Ever_Married | Work_Type | Residence_Type | Avg_Glucose_Level | BMI | Smoking_status | Stroke | 
+| Gender | Age | Hypertension | Heart_Disease | Ever_Married | Work_Type | Residence_Type | Avg_Glucose_Level | BMI | Smoking_status | Stroke | 
 ---
-| 0 or 1 | Int | 0 or 1 | 0 or 1 | 0 or 1 | 0 - 2 | 0 or 1 | Int | Float | Float | 0 - 2 | 0 or 1 |
+| 0 or 1 | Int | 0 or 1 | 0 or 1 | 0 or 1 | 0 or 1 | 0 - 2 | 0 or 1 | Int | Float | Float | 0 - 2 | 0 or 1 |
+---
 
-Work_Type(2) = Goverment-employed
-Work_Type(1) = Private
-Work_Type(0) = Self-employed
+# Gender 
+Gender(0) = Male
+Gender(1) = Woman
 
-Smoking_status(4) = Unknown
-Smoking_status(2) = Smokes
-Smoking_status(1) = Formerly Smokes
+# Hypertension
+Hypertension(0) = ?
+Hypertension(1) = ?
+
+# Heart_Disease
+Heart_Disease(0) = ?
+Heart_Disease(1) = ?
+
+# Ever_Married
+Ever_Married(0) = Yes
+Ever_Married(1) = No
+
+# Work_Type
+Work_Type(0) = Child
+Work_Type(1) = Never_worked
+Work_Type(2) = Self-employed
+Work_Type(3) = Private
+Work_Type(4) = Goverment-employed
+
+# Residence_Type 
+Residence_Type(0) = Urban
+Residence_Type(1) = Rural
+
+# Smoking_status 
 Smoking_status(0) = Never Smokes
+Smoking_status(1) = Formerly Smokes
+Smoking_status(2) = Smokes
+Smoking_status(3) = Unknown
