1bbddbae720d4c79106ad277e92d6a27714f9c94
Phong Do <Donguyenphong@icloud.com>
2024-10-21 19:51:37 -0700

    Began on data processing 1. Turned the CSV into a dataframe named stroke 2. Removed all N/A

--- DataProcess.py
+++ DataProcess.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import numpy as np
 
+# Turns the CSV into a dataframe named stroke
 stroke = pd.read_csv('healthcare-dataset-stroke-data - healthcare-dataset-stroke-data.csv')
-stroke['id']
+
+# Drops IDs as it is non important for the ML
+stroke = stroke.drop(['id'], axis = 1)
+
+# Removing N/A from the BMI data and inputing the mean
+Bmi_Mean = stroke['bmi'].mean()
+print(Bmi_Mean)
+stroke.replace(['N/A'], np.nan, inplace=False)
+stroke['bmi'] = stroke['bmi'].fillna(Bmi_Mean, inplace = False)
+
+#stroke['work_type'] = pd.Categorical(stroke['work_type'])
+#print(stroke['work_type'].code)
+
---
+++ healthcare-dataset-guide.mk
@@ -0,0 +1,12 @@
+| Gender | Age | Hypertension | Ever_Married | Work_Type | Residence_Type | Avg_Glucose_Level | BMI | Smoking_status | Stroke | 
+---
+| 0 or 1 | Int | 0 or 1 | 0 or 1 | 0 or 1 | 0 - 2 | 0 or 1 | Int | Float | Float | 0 - 2 | 0 or 1 |
+
+Work_Type(2) = Goverment-employed
+Work_Type(1) = Private
+Work_Type(0) = Self-employed
+
+Smoking_status(4) = Unknown
+Smoking_status(2) = Smokes
+Smoking_status(1) = Formerly Smokes
+Smoking_status(0) = Never Smokes
